ebb6e26d2a856710bf25606f95c369d734eff3dc
duochanatharvard <duochan@g.harvard.edu>
2018-08-13 14:43:43 -0400

    Allow revert hotCD

--- hotCD.m
+++ hotCD.m
@@ -1,6 +1,12 @@
-function col = hotCD(num)
+function col = hotCD(num,rev)
     
     if ~exist('num','var') num = 12; end
-    col = colormap_CD([0.25 0.9],[1 .35],[0],num);
+    if ~exist('rev','var') rev = 0; end    
+    
+    if rev == 0,
+        col = colormap_CD([0.25 0.9],[1 .35],[0],num);
+    else
+        col = colormap_CD([0.9 0.25],[0.35 1],[0],num); 
+    end
 
 end
