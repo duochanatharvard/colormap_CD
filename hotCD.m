46bf6963cc484b7487b7505e4e6368a860231cb6
duochanatharvard <duochan@g.harvard.edu>
2018-08-14 23:34:29 -0400

    Update hotCD.m

--- hotCD.m
+++ hotCD.m
@@ -20,12 +20,6 @@ function col = hotCD(num,hue)
     % ********************************************************************* 
     if ~exist('num','var')  num = 12; end
     if ~exist('hue','var') name = 'r'; end 
-    
-    if rev == 0,
-        col = colormap_CD([0.25 0.9],[1 .35],[0],num);
-    else
-        col = colormap_CD([0.9 0.25],[0.35 1],[0],num); 
-    end
 
     % *********************************************************************
     % Generate colormap
